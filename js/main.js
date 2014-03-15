c0eed7dbaf9f517171e17238b8835cd0a4b91d66
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2014-03-15 19:27:52 -0300

    Version 0.1.5

---
+++ js/main.js
@@ -0,0 +1,324 @@
+$(document).ready(function() {
+	
+	var directUC = (function(){
+		
+		var directUC = {},
+			self = directUC;
+
+		self.form_url = function(service) {
+			switch(service) {
+				case "siding":
+					return self.url_siding();
+					break;
+				case "webcursos":
+					return self.url_webcursos();
+					break;
+				case "portal":
+					return self.url_portal();
+					break;
+				case "mailuc":
+					return self.url_mailuc();
+					break;
+				case "aleph":
+					return self.url_aleph();
+					break;
+				case "labmat":
+					return self.url_labmat();
+					break;
+			}
+		}
+
+		self.form_data = function(user, pass, service) {
+			switch(service) {
+				case "siding":
+					return self.data_siding(user, pass);
+					break;
+				case "webcursos":
+					return self.data_webcursos(user,pass)
+					break;
+				case "portal":
+					return self.data_portal(user, pass);
+					break;
+				case "mailuc":
+					return self.data_mailuc(user,pass);
+					break;
+				case "aleph":
+					return self.data_aleph(user, pass);
+					break;
+				case "labmat":
+					return self.data_labmat(user, pass);
+					break;
+			}
+		}
+
+		self.redirect = function(service) {
+				switch(service) {
+					case "siding":
+						return self.url_redirect_siding();
+						break;
+					case "webcursos":
+						return "http://webcurso.uc.cl/portal";
+						break;
+					case "portal":
+						return self.url_portal();
+						break;
+					case "mailuc":
+						return self.url_mailuc();
+						break;
+					case "aleph":
+						return self.url_redirect_aleph();
+						break;
+					case "labmat":
+						return self.url_labmat();
+						break;
+				}	
+		}
+
+		self.url_siding = function() {
+							return "https://intrawww.ing.puc.cl/siding/index.phtml";
+						}
+
+		self.url_redirect_siding = function() {
+									var redirect_url;
+
+									if(localStorage.getItem("option-siding-cursos") == true) {
+										redirect_url = "https://intrawww.ing.puc.cl/siding/dirdes/ingcursos/cursos/vista.phtml";
+									} else {
+										redirect_url = self.url_siding();
+									}
+
+									return redirect_url;
+								}
+
+		self.data_siding = function(user, pass) {
+
+								var data_user = (localStorage.getItem("option-siding-login") == true && localStorage.getItem("option-siding-login-user") ) ? localStorage.getItem("option-siding-login-user") : user,
+									data_pass = (localStorage.getItem("option-siding-login") == true && localStorage.getItem("option-siding-login-pass") ) ? localStorage.getItem("option-siding-login-pass") : pass;
+
+									console.log(data_user+" : "+data_pass);
+
+
+								return {"login": data_user, "passwd": data_pass}	
+							}
+
+
+		self.url_webcursos = function() {
+								return "http://webcurso.uc.cl/direct/session";
+							}
+
+		self.data_webcursos = function(user, pass) {
+									return {"_username": user, "_password": pass};
+								}
+
+		/* TODO
+			- No he podido capturar ningún dato del formulario.
+		*/
+		self.url_mailuc = function() {
+								return "https://webaccess.uc.cl/simplesaml/";
+							}
+
+		self.data_mailuc = function(user,pass) {
+								var aState;
+
+								$.ajax({
+									async: false,
+									type: "GET",
+									url: self.url_mailuc(),
+									success: function(data) {
+											}		
+								});
+							}
+
+		self.url_aleph = function() {
+								var url;
+								$.ajax({
+									async: false,
+									type: "GET",
+									url: "http://aleph.uc.cl/F",
+									success: function(data) {
+												url = $(data).filter("form").attr("action");
+											}
+								});
+
+								return url;
+							}
+
+		self.url_redirect_aleph = function() {
+									if(localStorage.getItem("option-aleph-profile") == true) {
+										return "http://aleph.uc.cl/F/?func=bor-info";
+									} else {
+										return "http://aleph.uc.cl/";
+									}
+								}
+
+		self.data_aleph = function (user, pass) {
+								return {"func": "login-session",
+										"login_source": "LOGIN-BOR",
+										"bor_id": user,
+										"bor_verification": pass
+									}
+							}
+
+		self.url_portal = function() {
+								return "https://sso.uc.cl/cas/login?service=https%3A%2F%2Fportal.uc.cl%2Fc%2Fportal%2Flogin";
+							}
+
+		self.data_portal = function (user, pass) {
+
+								var execution, lt, data_obj;
+
+								$.ajax({
+									async: false,
+									type: "GET",
+									url: self.url_portal(),
+									success: function(data) {
+												execution = $(data).find('input[name=execution]').val();
+												lt = $(data).find('input[name=lt]').val();
+												
+												data_obj = {"username": user,
+														"password": pass,
+														"lt": lt,
+														"execution": execution,
+														"_eventId": "submit",
+														"submit": "Iniciar Sesión"
+														};
+											}
+								});
+								return data_obj;
+							}
+		self.url_labmat = function() {
+								return "http://labmat.puc.cl/index.php";
+							}
+
+		self.data_labmat = function(user, pass) {
+
+								var dominio = (localStorage.getItem("option-labmat-dominio") == true) ? "@mat.puc.cl" : "@uc.cl";
+								return {"accion": "ingreso",
+										"usuario": user+dominio,
+										"clave": pass}								
+							}
+
+		self.login = function(user, pass, service) {
+						var url = self.form_url(service),
+							form_data = self.form_data(user,pass, service),
+							redirect_url = self.redirect(service);
+						$.post(
+							url,
+							form_data,
+				    		function(data, status, xhr) {
+				    			localStorage.setItem("user", user);
+				    			localStorage.setItem("pass", pass);
+				    			if(service == "siding") {
+									var expired = $(data).find("noscript").length > 0;
+									
+									if (expired) {
+										self.login(user, pass, service);
+									}
+				    			}
+				    			if (localStorage.getItem("option-sametab") == true) {
+				    				chrome.tabs.update({'url': redirect_url});
+				    				window.close();
+				    			} else {
+				    				chrome.tabs.create({'url': redirect_url});				    				
+				    			}
+				    		}
+				    	);
+					}
+
+		return directUC;
+
+	})();
+
+	var user = localStorage.getItem("user"),
+		pass = localStorage.getItem("pass"),
+		siding_user = localStorage.getItem("option-siding-login-user"),
+		siding_pass = localStorage.getItem("option-siding-login-pass"),
+		// hide_form = localStorage.getItem("hide-form"),
+		activate_siding = localStorage.getItem("activate-siding"),
+		activate_labmat = localStorage.getItem("activate-labmat"),
+		activate_aleph = localStorage.getItem("activate-aleph"),
+		activate_portal = localStorage.getItem("activate-portal") || 1,
+		activate_webcursos = localStorage.getItem("activate-webcursos") || 1;
+
+	if(activate_siding == true) {
+		var service = "siding";
+		$("input#"+service).css('display','inline-block');
+		$("label[for="+service+"]").css('display','inline-block');
+	}
+
+	if(activate_labmat == true) {
+		var service = "labmat";
+		$("input#"+service).css('display','inline-block');
+		$("label[for="+service+"]").css('display','inline-block');
+	}
+	
+	if(activate_aleph == true) {
+		var service = "aleph";
+		$("input#"+service).css('display','inline-block');
+		$("label[for="+service+"]").css('display','inline-block');
+	}
+
+	if(activate_portal != true) {
+		var service = "portal";
+		$("input#"+service).hide();
+		$("label[for="+service+"]").hide();
+	}
+
+	if(activate_webcursos != true) {
+		var service = "webcursos";
+		$("input#"+service).hide();
+		$("label[for="+service+"]").hide();
+	}
+
+	if(user) {
+		$("#user").val(user).blur();
+		$("#password").val(pass).blur();
+		$(".service-inputs input").first().focus();
+	}
+	
+	/* if(hide_form == true) {
+		$("#content form .text-inputs").hide();
+	} */
+
+	$('#content form').keydown(function(event) {
+		if(event.which == 13) {
+			$(this).submit();
+		}
+	});
+
+	$("#content form .service-inputs label").click(function(e) {
+		e.preventDefault();
+		var label_for = $(this).attr('for');
+		$('input#'+label_for).click();
+		$("#content form").submit();	
+	});
+
+	$("#content form").submit(function(e) {
+		e.preventDefault();
+		var user = $(this).find("#user").val(),
+			pass = $(this).find("#password").val(),
+			service = $(this).find("input[name=service]:checked").val();
+		if(user && pass && service) {
+			$("#content, #popup .loader").addClass("gone");
+			directUC.login(user, pass, service);
+		}
+	});
+
+	$("#content form .service-inputs input").focus(function() {
+  		$(this).click();
+	});
+
+	$("#content form .service-inputs input").change(function() {
+		$('.labelchecked').removeClass('labelchecked');
+  		$("label[for='" + 	$("#content form .service-inputs input:checked").attr('id') + "']").addClass("labelchecked");
+	});
+
+	$("#popup .options-link").click(function (e) {
+		e.preventDefault();
+		chrome.tabs.create({'url': chrome.extension.getURL("options.html")});
+	});
+
+	$("#popup #close").click(function(event) {
+		window.close();
+	});
+});
---
+++ js/options.js
@@ -0,0 +1,257 @@
+$(document).ready(function() {
+	
+	// Variables
+		// Usuario
+	var user = localStorage.getItem("user"),
+		pass = localStorage.getItem("pass"),
+		remembered = (user != null),
+
+		option_sametab = localStorage.getItem("option-sametab"),
+		//hide_form = localStorage.getItem("hide-form"),
+
+
+
+		// Servicios
+		activate_siding = localStorage.getItem("activate-siding"),
+		option_siding_cursos = localStorage.getItem("option-siding-cursos"),
+		option_siding_login = localStorage.getItem("option-siding-login"),
+		option_siding_login_user = localStorage.getItem("option-siding-login-user"),
+		option_siding_login_pass = localStorage.getItem("option-siding-login-pass"),
+		siding_login_user = $("#option-siding-login-user"),
+		siding_login_pass = $("#option-siding-login-pass"),
+		siding_login_showpass = $("#options .option-row #option-siding-login-box .option-siding-login-showpassword"),
+
+		activate_labmat = localStorage.getItem("activate-labmat"),
+		option_labmat_dominio = localStorage.getItem("option-labmat-dominio"),
+
+		activate_aleph = localStorage.getItem("activate-aleph"),
+		option_aleph_profile = localStorage.getItem("option-aleph-profile"),
+
+		activate_portal = localStorage.getItem("activate-portal") || 1,
+		activate_webcursos = localStorage.getItem("activate-webcursos") || 1;
+
+	// Estado Inicial
+
+	if(remembered) {
+		// Usuario
+		$(".current-user").text(user);
+		$(".login-alert").hide();
+
+		$("#option-sametab").prop('checked', option_sametab == true);
+
+		// $("#hide-form").attr('checked', hide_form == true);
+
+
+		// Servicios
+		
+		// SIDING
+		$(".services-options #activate-siding").prop('checked', activate_siding == true);
+		$(".services-options #option-siding-cursos")
+			.attr('disabled', activate_siding == false)
+			.prop('checked', activate_siding == true && option_siding_cursos == true);
+		$(".services-options #option-siding-login")
+			.attr('disabled', activate_siding == false)
+			.prop('checked', activate_siding == true && option_siding_login == true);
+
+		if (option_siding_login != true) {
+			$("#option-siding-login-box").hide();
+		}
+
+		$("#option-siding-login-user").attr('placeholder', user);
+		$("#option-siding-login-pass").attr('placeholder', "sin cambiar");
+
+
+		if (option_siding_login_user) {
+			siding_login_user.val(option_siding_login_user);
+		}
+
+		if (option_siding_login_pass) {
+			siding_login_pass.val(option_siding_login_pass);
+			siding_login_showpass.addClass('active');
+
+		}
+
+		$(".services-options #activate-labmat").prop('checked', activate_labmat == true);
+		$(".services-options #option-labmat-dominio")
+			.attr('disabled', activate_labmat == false)
+			.prop('checked', activate_labmat == true && option_labmat_dominio == true);
+
+		$(".services-options #activate-aleph").prop('checked', activate_aleph == true);
+		$(".services-options #option-aleph-profile")
+			.attr('disabled', activate_aleph == false)
+			.prop('checked', activate_aleph == true && option_aleph_profile == true);
+		
+		$(".services-options #activate-portal").prop('checked', activate_portal == true);
+		$(".services-options #activate-webcursos").prop('checked', activate_webcursos == true);
+
+
+
+	} else {
+		resetOptions();
+	}
+
+	// Estado dinamico
+
+	siding_login_showpass.hover(function() {
+		$("#option-siding-login-pass").attr('type', 'text');
+	}, function() {
+		$("#option-siding-login-pass").attr('type', 'password');
+	});
+
+	$(".forget-user").click(function(event) {
+		resetOptions();
+	});
+
+	$.each([siding_login_user, siding_login_pass], function(index, el) {
+		$(this).keyup(function(event) {
+			var valor = $(this).val();
+			if($(this).is(siding_login_user)) {
+				if (valor.length > 0) {
+					localStorage.setItem("option-siding-login-user", valor);
+				} else {
+					localStorage.removeItem("option-siding-login-user");
+				}
+			} else if ($(this).is(siding_login_pass)) {
+				if (valor.length > 0) {
+					siding_login_showpass.addClass('active');
+					localStorage.setItem("option-siding-login-pass", valor);
+				} else {
+					siding_login_showpass.removeClass('active');
+					localStorage.removeItem("option-siding-login-pass");
+				}
+			}
+		});
+	});
+
+	$("input[type=checkbox]").change(function(event) {
+
+		if( $('.services-options input[id^="activate"]:checked').length == 0) {
+			event.preventDefault();
+			$(this).prop('checked', true);
+			return;
+		}
+
+		/*
+		 if ($(this).is("#hide-form")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("hide-form", 1);
+			} else {
+				localStorage.setItem("hide-form", 0);
+			}
+		} else */
+
+		if ($(this).is("#option-sametab")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("option-sametab", 1);
+			} else {
+				localStorage.setItem("option-sametab", 0);
+			}
+		} else if($(this).is("#activate-siding")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("activate-siding", 1);
+				$("#option-siding-cursos, #option-siding-login").removeAttr("disabled");
+			} else {
+				localStorage.setItem("activate-siding", 0);
+				localStorage.setItem("option-siding-cursos", 0);
+				localStorage.setItem("option-siding-login", 0);
+				$("#option-siding-login-box").slideUp(200);
+				$("#option-siding-cursos, #option-siding-login").attr("disabled","disabled").prop('checked', false);
+			}
+
+		} else if ($(this).is("#option-siding-cursos")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("option-siding-cursos", 1);
+			} else {
+				localStorage.setItem("option-siding-cursos", 0);
+			}
+		} else if ($(this).is("#option-siding-login")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("option-siding-login", 1);
+				$("#option-siding-login-box").slideDown(200);
+			} else {
+				localStorage.setItem("option-siding-login", 0);
+				$("#option-siding-login-box").slideUp(200);
+			}
+		} else if ($(this).is("#activate-labmat")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("activate-labmat", 1);
+				$("#option-labmat-dominio").removeAttr("disabled");
+
+			} else {
+				localStorage.setItem("activate-labmat", 0);
+				localStorage.setItem("option-labmat-dominio", 0);
+
+				$("#option-labmat-dominio").attr("disabled","disabled").prop('checked', false);
+			}
+		} else if ($(this).is("#option-labmat-dominio")){
+			if($(this).is(":checked")) {
+				localStorage.setItem("option-labmat-dominio", 1);
+			} else {
+				localStorage.setItem("option-labmat-dominio", 0);
+			}
+		} else if ($(this).is("#activate-aleph")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("activate-aleph", 1);
+				$("#option-aleph-profile").removeAttr("disabled");
+
+			} else {
+				localStorage.setItem("activate-aleph", 0);
+				localStorage.setItem("option-aleph-profile", 0);
+
+				$("#option-aleph-profile").attr("disabled","disabled").prop('checked', false);
+			}
+		} else if ($(this).is("#option-aleph-profile")){
+			if($(this).is(":checked")) {
+				localStorage.setItem("option-aleph-profile", 1);
+			} else {
+				localStorage.setItem("option-aleph-profile", 0);
+			}
+		} else if ($(this).is("#activate-portal")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("activate-portal", 1);
+
+			} else {
+				localStorage.setItem("activate-portal", 0);
+			}
+		} else if ($(this).is("#activate-webcursos")) {
+			if($(this).is(":checked")) {
+				localStorage.setItem("activate-webcursos", 1);
+
+			} else {
+				localStorage.setItem("activate-webcursos", 0);
+			}
+		}
+	});
+
+
+	function resetOptions() {
+
+		// Local Storage
+		localStorage.clear();
+
+		// Usuario
+		$(".current-user").text("Ninguno").addClass('no-user');
+		$(".forget-user").hide();
+
+
+		// Desactivar Servicios
+		var uncheck_forms = ["#hide-form",
+							".services-options input[id*=siding]",
+							".services-options input[id*=labmat]",
+							".services-options input[id*=aleph]"]
+		$(uncheck_forms.join(", ")).prop('checked', false);
+
+		siding_login_pass.val("");
+		siding_login_user.val("");
+		$("#option-siding-login-box").hide();
+
+		// Activar Portal y Webcursos
+		$('.services-options #activate-portal, .services-options #activate-webcursos').prop('checked', true);
+
+		$("input").each(function() {
+			$(this).attr('disabled', 'disabled');		
+		});
+
+		$(".login-alert").fadeIn();
+	}
+});
