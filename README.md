98e7dc360de9cebc91a52ce39013d8ff48f96659
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2014-10-04 14:17:19 -0300

    Version 0.1.8

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-directUC
-========
+#directUC
+
+Extensión para Google Chrome que permite a alumnos de la Pontificia Universidad Católica de Chile el ingreso directo a diferentes servicios ofrecidos por esta.
+
---
+++ js/siding-bar.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function() {
+
+	var datos;
+	chrome.runtime.sendMessage("getData", function(response) {
+		if(response.user && response.user != "") {
+			datos = response;
+			mostrarMensaje();
+		}
+	});
+
+	function mostrarMensaje() {
+		jQuery("head").append("<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>");
+		jQuery("#header").prepend('<div style="display:none;" class="directuc-siding-bar">Ingresar al SIDING como <span class="user">'+datos.user+ '</span></div>');
+		jQuery(".directuc-siding-bar").fadeIn();
+	}
+
+	jQuery(document).on('click', '.directuc-siding-bar', function(e) {
+		e.preventDefault();
+		jQuery("#user-siding").val(datos.user);
+		jQuery("#password-siding").val(datos.pass);
+		var submit = jQuery("#form-siding input[type=submit]");
+		submit.click();
+		jQuery("body").fadeOut();
+	});
+
+});
