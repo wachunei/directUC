42e3cf882915db6c33fa735a62cb7ed6e171e8de
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2016-02-01 08:37:24 -0300

    Fix debugger line

--- js/background.js
+++ js/background.js
@@ -319,7 +319,6 @@ var directUC = (function() {
       req.open('POST', url, true);
       req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
       req.onreadystatechange = function() {
-        debugger;
         if (req.readyState == 4 && req.status >= 200) {
           if (service === self.services.siding ) {
             var parser = new DOMParser();
