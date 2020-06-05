8877a76cc00ad8052afb18f7d81549c4391fd4bd
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2020-06-05 10:56:40 -0400

    Remove custom alias middleware and use redux-thunk
    like normal people would do

--- src/js/store/aliases.js
+++ src/js/store/aliases.js
@@ -2,7 +2,9 @@ import { actions } from "../redux/services";
 
 const serviceActionsHandler = (handler) =>
   Object.keys(actions).reduce((acc, key) => {
-    acc[actions[key].callAction] = (...params) => handler(key, ...params);
+    acc[actions[key].callAction] = (action) => (dispatch, getState) => {
+      return handler(key, action, { dispatch, getState });
+    };
     return acc;
   }, {});
 
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,31 +1,20 @@
 import { createStore, compose, applyMiddleware } from "redux";
+import { alias } from "webext-redux";
+import ReduxThunk from "redux-thunk";
 import { logger } from "redux-logger";
-import reducers from "../redux/reducers";
-// import { alias } from "webext-redux";
 
-const middlewares = [];
+import reducers from "../redux/reducers";
+
+const middlewares = [ReduxThunk];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-const customAliasMiddleware = (aliases) => ({ dispatch, getState }) => (
-  next
-) => (action) => {
-  const alias = aliases[action.type];
-  if (alias) {
-    return alias(action, { dispatch, getState });
-  }
-
-  return next(action);
-};
-
 const configureStore = (aliases) =>
   createStore(
     reducers,
-    compose(
-      applyMiddleware(...[customAliasMiddleware(aliases), ...middlewares])
-    )
+    compose(applyMiddleware(...[alias(aliases), ...middlewares]))
   );
 
 export default configureStore;
