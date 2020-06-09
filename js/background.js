74373500e5c7b80a2c63c1cee32146a02986aa71
Agustín Covarrubias <agucova@uc.cl>
2020-06-09 13:46:58 -0400

    Fix: Añadir fuzzy finder Arregla el bug reportado en DCConfesión #9134 ;) Añade fuse.js para ese propósito.

--- js/background.js
+++ js/background.js
@@ -411,7 +411,7 @@ var omniCanvas = {
 var omniSiding = {
   content: 'SIDING',
   description: '<match>SIDING</match> <dim>Ir a tu SIDING</dim>',
-  matchStrings: ['siding']
+  matchStrings: ['siding', 'ing']
 };
 var omniLabmat = {
   content: 'LABMAT',
@@ -431,8 +431,9 @@ var omniWebcursos = {
 var omniMailuc = {
   content: 'Correo UC',
   description: '<match>Correo UC</match> <dim>Ir a Correo UC</dim>',
-  matchStrings: ['correo', 'mail']
+  matchStrings: ['correo', 'mail', 'gmail']
 };
+
 
 chrome.omnibox.onInputChanged.addListener(
   function (text, _suggest) {
@@ -460,29 +461,23 @@ chrome.omnibox.onInputChanged.addListener(
       suggestions.push(omniMailuc);
     }
 
-    var suggested = false;
-
-    suggestions.forEach(function (item) {
-      var simpleText = text.toLowerCase().replace(/\s+/g, '');
-      item.matchStrings.forEach(function (matchString) {
-        if ((matchString.indexOf(simpleText) != -1) && !suggested) {
-          suggested = true;
-          suggestedItem = item;
-          chrome.omnibox.setDefaultSuggestion({
-            description: item.description
-          });
-        }
-      });
-    });
-
-    if (suggested) {
-      var index = suggestions.indexOf(suggestedItem);
-      suggestions.splice(index, 1);
+    // Fuzzy matcher
+    const searchOptions = {
+      includeScore: false,
+      isCaseSensitive: false,
+      keys: ['matchStrings'],
+      distance: 200,
+      includeMatches: false,
+      findAllMatches: true
     }
-
-    //suggest(suggestions);
-
-
+    var fuse = new Fuse(suggestions, searchOptions);
+    var searchResults = fuse.search(text);
+    if (searchResults.length > 0) {
+      var suggestedItem = searchResults[0]['item'];
+      chrome.omnibox.setDefaultSuggestion({
+        description: suggestedItem.description
+      });
+    }
   });
 
 chrome.omnibox.onInputEntered.addListener(
