ed0b69adb05e39be1ed8a91ee700d012287f3ff1
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2020-09-09 16:49:23 -0300

    Use titleCase false in react-ga

--- src/js/analytics/next.js
+++ src/js/analytics/next.js
@@ -1,9 +1,12 @@
 import ReactGA from "react-ga";
 
-const trackingId =
-  process.env.NODE_ENV === "development" ? "UA-177532719-1" : "UA-62971405-1";
+const isDebug = process.env.NODE_ENV === "development";
+const trackingId = isDebug ? "UA-177532719-1" : "UA-62971405-1";
 
-ReactGA.initialize(trackingId);
+ReactGA.initialize(trackingId, {
+  titleCase: false,
+});
+
 ReactGA.ga("set", "checkProtocolTask", null);
 
 export default {
